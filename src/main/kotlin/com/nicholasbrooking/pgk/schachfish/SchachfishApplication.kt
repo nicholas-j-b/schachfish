9db68f0826a05bef0e0a25d08c4b0fce556122a9
nick <nicholas.j.brooking@gmail.com>
2020-08-07 11:35:25 +0200

    app init

---
+++ src/main/kotlin/com/nicholasbrooking/pgk/schachfish/SchachfishApplication.kt
@@ -0,0 +1,11 @@
+package com.nicholasbrooking.pgk.schachfish
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SchachfishApplication
+
+fun main(args: Array<String>) {
+	runApplication<SchachfishApplication>(*args)
+}
---
+++ src/test/kotlin/com/nicholasbrooking/pgk/schachfish/SchachfishApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nicholasbrooking.pgk.schachfish
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SchachfishApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
